9a793f8c6df02ad4b5007601007f6e523693e809
whatsacomputertho <ebalcik71@gmail.com>
2023-10-28 21:20:13 -0400

    Implement bare minimum elevator struct, add elevator sim functionality

--- src/elevator.rs
+++ src/elevator.rs
@@ -4,7 +4,7 @@
  * - energy_up (f64): Base energy spent per floor when empty & moving up
  * - energy_down (f64): Base energy spent per floor when empty & moving down
  * - energy_coef (f64): Multiplier for calculating energy spent while traveling with people
- * - floor_on (i32): The floor that the elevator is currently on
+ * - floor_on (usize): The floor that the elevator is currently on
  * - moving_up (bool): If true, the elevator is moving up, else it is moving down
  * - stopped (bool): If true, the elevator is stopped, else it is moving
  */
@@ -12,7 +12,7 @@ pub struct Elevator {
     energy_up: f64,
     energy_down: f64,
     energy_coef: f64,
-    floor_on: i32,
+    floor_on: usize,
     moving_up: bool,
     stopped: bool
 }
@@ -31,7 +31,7 @@ pub struct Elevator {
         energy_up: energy_up,
         energy_down: energy_down,
         energy_coef: energy_coef,
-        floor_on: 0_i32,
+        floor_on: 0_usize,
         moving_up: false,
         stopped: true
     }
@@ -57,10 +57,72 @@ impl Elevator {
         let energy_spent = if self.stopped {
                 0.0_f64
             } else if self.moving_up {
-                self.energy_up + self.energy_coef * (num_people as f64)
+                self.energy_up + (self.energy_coef * (num_people as f64))
             } else {
-                self.energy_down + self.energy_coef * (num_people as f64)
+                self.energy_down + (self.energy_coef * (num_people as f64))
             };
         energy_spent
+    }
+
+    /** get_floor_on function
+     *
+     * Determine the floor the elevator is on.
+     * Return the floor_on usize.
+     */
+     pub fn get_floor_on(&mut self) -> usize {
+        self.floor_on
+    }
+
+    /** is_stopped function
+     *
+     * Determine whether the elevator is stopped.
+     * Return the stopped boolean.
+     */
+    pub fn is_stopped(&mut self) -> bool {
+        self.stopped
+    }
+
+    /** is_moving_up function
+     *
+     * Determine whether the elevator is moving up.
+     * Return the moving_up boolean.
+     */
+    pub fn is_moving_up(&mut self) -> bool {
+        self.moving_up
+    }
+
+    /** update_floor function
+     *
+     * Update the floor the elevator is on.
+     * Increment or decrement the floor_on usize based on whether
+     * the elevator is stopped and/or moving up.
+     */
+     pub fn update_floor(&mut self) -> usize {
+        self.floor_on = if self.stopped {
+            self.floor_on
+        } else if self.moving_up {
+            self.floor_on + 1_usize
+        } else {
+            self.floor_on - 1_usize
+        };
+        self.floor_on
+    }
+
+    /** set_stopped function
+     *
+     * Set whether the elevator is stopped.
+     * Update the stopped bool with the input bool.
+     */
+    pub fn set_stopped(&mut self, is_stopped: bool) {
+        self.stopped = is_stopped;
+    }
+
+    /** set_moving_up function
+     *
+     * Set whether the elevator is moving up.
+     * Update the moving_up bool with the input bool.
+     */
+    pub fn set_moving_up(&mut self, is_moving_up: bool) {
+        self.moving_up = is_moving_up;
     }
 }
--- src/person.rs
+++ src/person.rs
@@ -1,4 +1,5 @@
 //Import libraries
+use std::fmt;
 use rand::Rng;
 use rand::distributions::{Distribution, Standard, Uniform, Bernoulli};
 use rand::seq::SliceRandom;
@@ -12,8 +13,8 @@ use rand::seq::SliceRandom;
  * then the person is waiting for an elevator.
  */
 pub struct Person {
-    floor_on: i32,
-    floor_to: i32,
+    floor_on: usize,
+    floor_to: usize,
     dst_out: Bernoulli,
     is_on_elevator: bool
 }
@@ -30,11 +31,11 @@ pub struct Person {
  * The num_floors and rng instance are used together to randomly
  * generate that person's destination floor on instatiation.
  */
-pub fn from(p_out: f64, num_floors: i32, mut rng: &mut impl Rng) -> Person {
-    let dst_to = Uniform::new(0, num_floors);
-    let floor_to: i32 = dst_to.sample(&mut rng);
+pub fn from(p_out: f64, num_floors: usize, mut rng: &mut impl Rng) -> Person {
+    let dst_to = Uniform::new(0_usize, num_floors);
+    let mut floor_to: usize = dst_to.sample(&mut rng);
     Person {
-        floor_on: 0_i32,
+        floor_on: 0_usize,
         floor_to: floor_to,
         dst_out: Bernoulli::new(p_out).unwrap(),
         is_on_elevator: false
@@ -47,7 +48,7 @@ pub fn from(p_out: f64, num_floors: i32, mut rng: &mut impl Rng) -> Person {
  * and are callable via
  *
  * //Example
- * let my_person: Person = person::from(0.5_f64, 5_i32, &mut rng);
+ * let my_person: Person = person::from(0.5_f64, 5_usize, &mut rng);
  * let is_leaving: bool = my_person.is_leaving(&mut rng);
  */
 impl Person {
@@ -60,7 +61,7 @@ impl Person {
     pub fn is_leaving(&mut self, mut rng: &mut impl Rng) -> bool {
         let is_leaving: bool = self.dst_out.sample(&mut rng);
         if is_leaving {
-            self.floor_to = 0;
+            self.floor_to = 0_usize;
         }
         is_leaving
     }
@@ -91,18 +92,49 @@ impl Person {
     /** get_floor_on function
      *
      * Get the floor on which the person currently is.
-     * Return the floor_on i32.
+     * Return the floor_on usize.
      */
-    pub fn get_floor_on(&mut self) -> i32 {
+    pub fn get_floor_on(&mut self) -> usize {
         self.floor_on
     }
 
     /** get_floor_to function
      *
      * Get the floor to which the person would like to travel.
-     * Return the floor_on i32.
+     * Return the floor_on usize.
      */
-    pub fn get_floor_to(&mut self) -> i32 {
+    pub fn get_floor_to(&mut self) -> usize {
         self.floor_to
+    }
+
+    /** set_on_elevator function
+     *
+     * Set whether the person is on the elevator or not.
+     * Update the person's is_on_elevator bool with input bool.
+     */
+    pub fn set_on_elevator(&mut self, on_elevator: bool) {
+        self.is_on_elevator = on_elevator;
+    }
+
+    /** set_floor_on function
+     *
+     * Set the person's floor.
+     * Update the person's floor_on usize with input usize.
+     */
+     pub fn set_floor_on(&mut self, new_floor_on: usize) {
+        self.floor_on = new_floor_on;
+    }
+}
+
+impl fmt::Display for Person {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let display_str: String = if (self.floor_on != self.floor_to) && !self.is_on_elevator {
+            format!("Person  {} -> {} ", self.floor_on, self.floor_to)
+        } else if self.is_on_elevator {
+            format!("Person [{} -> {}]", self.floor_on, self.floor_to)
+        } else {
+            format!("Person  {}", self.floor_on)
+        };
+        f.write_str(&display_str)
     }
 }
